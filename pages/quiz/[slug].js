4ae35d2b5e62daf0c694a4e9aae4cea5504c17b8
William Frank Monroy Mamani <william_monroy777@hotmail.com>
2023-05-12 02:56:34 -0600

    feat: 🎸 Test, confetty and results page

--- pages/quiz/[slug].js
+++ pages/quiz/[slug].js
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styles from "../../styles/QuizQuestion.module.css";
+import { Button, Loading, Row, Text } from "@nextui-org/react";
+import { useRouter } from "next/router";
 
-const QuizQuestion = () => {
-  return <div></div>;
+const QuizQuestion = ({ slug, text, real }) => {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const handleSimulateFetch = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  };
+  useEffect(() => {
+    handleSimulateFetch();
+  }, []);
+
+  return (
+    <div className={styles.QuizQuestion}>
+      <Text h4>Question {slug}</Text>
+      {isLoading ? (
+        <Loading type="points" />
+      ) : (
+        <>
+          <Text>{text}</Text>
+          <Text>REAL: {real}</Text>
+          <Row>
+            <Button
+              color={"success"}
+              onClick={() => {
+                const score = window.localStorage.getItem("score");
+                window.localStorage.setItem(
+                  "score",
+                  real === 1 ? parseInt(score) + 10 : parseInt(score) - 5
+                );
+                setIsLoading(true);
+                handleSimulateFetch();
+                if (parseInt(slug) < 10) {
+                  router.push(`/quiz/${parseInt(slug) + 1}`);
+                } else {
+                  router.push(`/result`);
+                }
+              }}
+            >
+              Verdadero
+            </Button>
+            <Button
+              color={"error"}
+              onClick={() => {
+                const score = window.localStorage.getItem("score");
+                window.localStorage.setItem(
+                  "score",
+                  real === 1 ? parseInt(score) + -5 : parseInt(score) + 10
+                );
+                setIsLoading(true);
+                handleSimulateFetch();
+                if (parseInt(slug) < 10) {
+                  router.push(`/quiz/${parseInt(slug) + 1}`);
+                } else {
+                  router.push(`/result`);
+                }
+              }}
+            >
+              Falso
+            </Button>
+          </Row>
+        </>
+      )}
+    </div>
+  );
 };
+
+export async function getServerSideProps(context) {
+  const { slug } = context.query;
+  const real = Math.floor(Math.random() * 2);
+  if (real) {
+    const news = await fetch(`http://localhost:3000/api/news/`);
+    const articles = await news.json();
+    const text = await articles.articles[
+      parseInt(slug) * (Math.floor(Math.random() * 10) + 1)
+    ].description;
+    return {
+      props: {
+        slug,
+        text,
+        real,
+      },
+    };
+  } else {
+    const news = await fetch(`http://localhost:3000/api/news/`);
+    const articles = await news.json();
+    const url = await articles.articles[
+      parseInt(slug) * (Math.floor(Math.random() * 10) + 1)
+    ].url;
+    const fake = await fetch(`http://localhost:3000/api/fakeNews?link=${url}`);
+    const fakeNews = await fake.json();
+    const text = await fakeNews.choices[0].text;
+    console.log(text);
+    return {
+      props: {
+        slug,
+        text,
+        real,
+      },
+    };
+  }
+}
 
 export default QuizQuestion;
--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../../styles/QuizStart.module.css";
 import { Button, Card, Spacer, Text } from "@nextui-org/react";
+import { useRouter } from "next/router";
 
 const QuizStart = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    localStorage.setItem("score", 0);
+  }, []);
+
   return (
     <div className={styles.QuizStart}>
       <Card css={{ p: "1em" }}>
@@ -58,7 +65,12 @@ const QuizStart = () => {
           cuestionario!
         </Text>
         <Spacer y={1.5} />
-        <Button auto onClick={() => router.push("/quiz/1")}>
+        <Button
+          auto
+          onClick={() => {
+            router.push("/quiz/1");
+          }}
+        >
           Inicio
         </Button>
       </Card>
